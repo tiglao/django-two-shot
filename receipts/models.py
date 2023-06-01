02cf40fffdb7e0cd44e814de4a9825208990f696
Ellie <hello@ellietiglao.com>
2023-06-01 18:58:19 -0400

    Feature 3 complete

--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(
+        User, related_name="categories", on_delete=models.CASCADE
+    )
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=20)
+    owner = models.ForeignKey(
+        User, related_name="accounts", on_delete=models.CASCADE
+    )
+
+
+class Receipt(models.Model):
+    vendor = models.CharField(max_length=200)
+    total = models.DecimalField(max_digits=10, decimal_places=3)
+    tax = models.DecimalField(max_digits=10, decimal_places=3)
+    date = models.DateTimeField(auto_now_add=True)
+    purchaser = models.ForeignKey(
+        User, related_name="receipts", on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        "ExpenseCategory", related_name="receipts", on_delete=models.CASCADE
+    )
+    account = models.ForeignKey(
+        "Account",
+        related_name="receipts",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
